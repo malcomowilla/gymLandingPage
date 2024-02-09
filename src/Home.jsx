82a69847bb2746b8573659cf70cdeda4227ac83c
malcomowilla <owillamalcom@gmail.com>
2024-02-09 13:39:50 +0300

    responsiveness10

--- src/Home.jsx
+++ src/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
     sm:text-white sm:flex-col sm:justify-center sm:items-center sm:bg-black sm:h-[400px] sm:w-[600px] sm:relative
     sm:left-[70px] md:bg-transparent md:h-auto md:w-auto sm:top-[-40px]
      lg:space-y-[-30px] max-sm:justify-center max-sm:items-center max-sm:bg-black  max-sm:z-50  max-sm:text-white
-      max-sm:flex-col md:flex-row md:gap-11      max-sm:w-[400px] max-sm:h-[400px] max-sm:gap-[20px] 
+      max-sm:flex-col md:flex-row md:gap-11      max-sm:w-[350px] max-sm:h-[400px] max-sm:gap-[20px] 
       cursor-pointer  transition-all duration-500 ease-in max-sm:relative md:text-black md:text-xs lg:top-10
       md:translate-y-10 lg:translate-y-0 max-sm:right-[40px]
      '> 

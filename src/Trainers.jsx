b63a81537f404da7bda0a25d714706d497735b40
malcomowilla <owillamalcom@gmail.com>
2024-02-12 07:04:16 +0300

    responsive so far so good18

--- src/Trainers.jsx
+++ src/Trainers.jsx
@@ -18,7 +18,7 @@ const Trainers = () => {
 
 
 <div className="grid border border-gray-200 rounded-lg  dark:border-gray-700 mb-[500px] absolute 
- md:grid-cols-2 gap-10  shadow-2xl  bg-white dark:bg-gray-800  max-sm:translate-y-[1000px] translate-y-[500px] ">
+ md:grid-cols-2 gap-10  shadow-2xl  bg-white dark:bg-gray-800  max-sm:translate-y-[1500px] translate-y-[500px] ">
     <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
         <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Very easy this was to integrate</h3>
